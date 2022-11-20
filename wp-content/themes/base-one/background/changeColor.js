e4c5b5de6ca03075b6cc0cda1c0f51b798fa32b3
calvin <garrixa6@gmail.com>
2022-11-19 20:35:43 -0500

    blob añadido

--- wp-content/themes/base-one/background/changeColor.js
+++ wp-content/themes/base-one/background/changeColor.js
@@ -1,21 +1,42 @@
-window.addEventListener("scroll", ()=>{
-   const anim = document.querySelectorAll("#section-anim")
-   const bg = document.querySelector(".panel")
-   const bgPosOne = anim[0].getBoundingClientRect().top
-   const bgPosTwo = anim[1].getBoundingClientRect().top
-   const bgPosTree = anim[2].getBoundingClientRect().top
+let blobColor
+
+window.addEventListener("scroll", () => {
+   const anim = document.querySelectorAll("#section-anim");
+   const bg = document.querySelector(".panel");
+   const bgPosOne = anim[0].getBoundingClientRect().top;
+   const bgPosTwo = anim[1].getBoundingClientRect().top;
+   const bgPosTree = anim[2].getBoundingClientRect().top;
    if (bgPosOne < 500) {
-      bg.classList.remove("bg-color-2")
-      bg.classList.remove("bg-color-3")
-      bg.classList.add("bg-color-1")
-   } 
+      blobColor = 0x5c1322
+   }
    if (bgPosTwo < 500) {
-      bg.classList.remove("bg-color-1")
-      bg.classList.remove("bg-color-3")
-      bg.classList.add("bg-color-2")
-   } 
+      blobColor = 0x000000
+   }
    if (bgPosTree < 500) {
-      bg.classList.remove("bg-color-2")
-      bg.classList.add("bg-color-3")
-   } 
-})
+      blobColor = 0xffffff
+   }
+ });
+
+
+ var radius = 8;
+ TweenMax.staggerFromTo('.blob', 4 ,{
+   cycle: {
+     attr:function(i) {
+       var r = i*90;
+       return {
+         transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
+       }      
+     }
+   }  
+ },{
+   cycle: {
+     attr:function(i) {
+       var r = i*90+360;
+       return {
+         transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
+       }      
+     }
+   },
+   ease:Linear.easeNone,
+   repeat:-1
+ });
